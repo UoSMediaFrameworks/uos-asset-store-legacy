a6982e23b709e5214cffea10f66264e4d6aab30d
Matt Schick <matt@schickm.com>
2014-12-17 10:54:11 -0600

    test cleanup

--- test/integration-tests.js
+++ test/integration-tests.js
@@ -39,47 +39,51 @@ describe('AssetStore', function () {
         store.close(done);
     });
 
-    describe('Upload an image without a token', function () {
-        it('should respond with a 401', function (done) {
-            request.post(uploadUrl)
-                .attach('image', xmpFile)
-                .expect(401, done);
+    describe(uploadUrl, function () {
+        beforeEach(function () {
+            this.request = request.post(uploadUrl);
+        });
+
+        describe('Upload an image without a token', function () {
+            it('should respond with a 401', function (done) {
+                this.request.attach('image', xmpFile)
+                    .expect(401, done);
+            });
+        });
+
+        describe('Upload an image without xmp', function () {
+            it('should respond with a 400', function (done) {
+                this.request.field('token', sessionId)
+                    .attach('image', noXmpFile)
+                    .expect(400, done);
+            });
+        });
+
+        describe('Upload an image with xmp', function () {
+            it('should respond with 200, and json object of tags and url', function (done) {
+                this.request.field('token', sessionId)
+                    .attach('image', xmpFile)
+                    .expect(200)
+                    .end(function(err, result) {
+                        var body = result.body;
+                        assert(result.type, 'application/json');
+                        assert(body.tags, 'no tags in response');
+                        assert(body.url, 'no url in response');
+                        done();
+                    });
+            });
+        });
+
+        describe('Make request without an image', function () {
+            it('should respond with a 400', function (done) {
+                this.request.send('token=' + sessionId)
+                    .end(function(err, res) {
+                        assert.equal(res.status, 400);
+                        done();
+                    });
+            });
         });
     });
 
-    describe('Upload an image without xmp', function () {
-        it('should respond with a 400', function (done) {
-            request.post(uploadUrl)
-                .field('token', sessionId)
-                .attach('image', noXmpFile)
-                .expect(400, done);
-        });
-    });
-
-    describe('Upload an image with xmp', function () {
-        it('should respond with 200, and json object of tags and url', function (done) {
-            request.post(uploadUrl)
-                .field('token', sessionId)
-                .attach('image', xmpFile)
-                .expect(200)
-                .end(function(err, result) {
-                    var body = result.body;
-                    assert(result.type, 'application/json');
-                    assert(body.tags, 'no tags in response');
-                    assert(body.url, 'no url in response');
-                    done();
-                });
-        });
-    });
-
-    describe('Make request without an image', function () {
-        it('should respond with a 400', function (done) {
-            request.post(uploadUrl)
-                .send('token=' + sessionId)
-                .end(function(err, res) {
-                    assert.equal(res.status, 400);
-                    done();
-                });
-        });
-    });
+    
 }); 
