8b167d45eccd007382b030803d5e676155a43ecb
Matt Schick <matt@schickm.com>
2014-12-17 15:44:07 -0600

    remove old run

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export AZURE_STORAGE_ACCOUNT=smaassetstore
-export AZURE_STORAGE_ACCESS_KEY=lV/aE9/yMXRG1m/LzaE6Gynt4PLn6HX/w81EKloGpn3f2sxpCIKjk+VRrDu+y4ifSA6BRZ+UERhvvfjgUnPwqw==
-export AZURE_STORAGE_CONTAINER=assetstoredev
-export MONGO_CONNECTION=mongodb://127.0.0.1:27017
-export PORT=4000
-
-$@
