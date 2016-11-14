d92b742b3b4afd4878c4b07656c985493eba784f
aaronphillips <aaronp261@gmail.com>
2016-11-14 18:28:04 +0000

    Updated max file size

--- src/asset-store.js
+++ src/asset-store.js
@@ -34,8 +34,8 @@ var AssetStore = function(ops) {
         next();
     });
 
-    app.use(bodyParser.json({limit: '100mb'}));
-    app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
+    app.use(bodyParser.json({limit: '1024mb'}));
+    app.use(bodyParser.urlencoded({limit: '1024mb', extended: true}));
 
     app.use(multer({
         dest: this._ops.uploadDir
