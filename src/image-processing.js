c8d9586a752d78579a2e5bd49b8e1a8f2f9ba557
Angel Petrov <angel.a.petrov2@gmail.com>
2016-12-28 15:44:45 +0000

    https://trello.com/c/W2I3Fchc/122-add-more-supported-media-types-to-the-upload-process-of-the-scene-editor
    fixed the problem with trying to create unsupported file type outputs when storing the thumbnail and resized image
    http://sharp.dimens.io/en/stable/api-output/

--- src/image-processing.js
+++ src/image-processing.js
@@ -6,15 +6,15 @@ var async = require('async');
 const thumbnailHeight = 90;
 const maxImageHeight = 768;
 
-module.exports = function() {
+module.exports = function () {
     return {
 
-        storeImage: function(ImageMediaObject, imageFilePath, imageFileName, callback) {
+        storeImage: function (ImageMediaObject, imageFilePath, imageFileName, callback) {
             var imob = new ImageMediaObject();
             imob.attach('image', { id: imob._id, path: imageFilePath, name: imageFileName }, function(error, result) {
                 if (error) throw error;
 
-                imob.save(function(error) {
+                imob.save(function (error) {
                     if (error) throw error;
 
                     callback(null, imob);
@@ -22,48 +22,62 @@ module.exports = function() {
             });
         },
 
-        uploadThumbnailImage: function(ImageMediaObject, imageFilePath, imageFileName, imageToUpload, callback) {
+        uploadThumbnailImage: function (ImageMediaObject, imageFilePath, imageFileName, imageToUpload, callback) {
             var self = this;
             var thumbnailImageFilePath = imageFilePath + "-thumbnail";
             var thumbnailImageFileName = "thumbnail-" + imageFileName;
+
+            /*
+             Angel Petrov 19/12/2016
+             http://sharp.dimens.io/en/stable/api-output/
+             toFile();
+             If an explicit output format is not selected, it will be inferred from the extension, with JPEG, PNG, WebP, TIFF, DZI, and libvips' V format supported
+             This was an issue when uploading gifs and svg images. Receiving an output format not supported error.
+             I added the png function to the function chain that will give us jpeg output options and treat make the output format valid
+             The final file will have a path and name that symbolises its source image but the actual type will be type/jpeg
+
+                This problem will persist for the resized version of the image as well, and a resized gif will not indeed be animated
+                as it will have a type of png or otherwise it will error
+
+             */
+
             imageToUpload
                 .resize(undefined, thumbnailHeight)
-                .toFile(thumbnailImageFilePath, function(err) {
-
-                    if(err) throw err;
-
+                .png()
+                .toFile(thumbnailImageFilePath, function (err) {
+                    if (err) throw err;
                     self.storeImage(ImageMediaObject, thumbnailImageFilePath, thumbnailImageFileName, callback);
-
                 });
         },
 
-        getImageMetadata: function(imageToUpload, callback){
+        getImageMetadata: function (imageToUpload, callback) {
             //meta data provides the data required for the requirements - size namely
             imageToUpload
-                .metadata(function(err, metadata){
-                    if(err) throw err;
-                    
+                .metadata(function (err, metadata) {
+                    if (err) throw err;
+
                     callback(metadata);
                 });
         },
 
-        uploadImage: function(ImageMediaObject, imageFilePath, imageFileName, imageToUpload, callback) {
+        uploadImage: function (ImageMediaObject, imageFilePath, imageFileName, imageToUpload, callback) {
             var self = this;
             async.parallel([
-               function(callback) {
-                   var resizedImageFilePath = imageFilePath + "-resized";
-                   var resizedImageFileName = "resized-" + imageFileName;
-                   imageToUpload
-                       .resize(undefined, maxImageHeight)
-                       .toFile(resizedImageFilePath , function(err){
-                           if(err) throw err;
-                           self.storeImage(ImageMediaObject, resizedImageFilePath, resizedImageFileName, callback);
-                       });
-               }, function(callback) {
+                function (callback) {
+                    var resizedImageFilePath = imageFilePath + "-resized";
+                    var resizedImageFileName = "resized-" + imageFileName;
+                    imageToUpload
+                        .resize(undefined, maxImageHeight)
+                        .png()
+                        .toFile(resizedImageFilePath, function (err) {
+                            if (err) throw err;
+                            self.storeImage(ImageMediaObject, resizedImageFilePath, resizedImageFileName, callback);
+                        });
+                }, function (callback) {
                     self.storeImage(ImageMediaObject, imageFilePath, imageFileName, callback);
                 }
-            ], function(err, results){
-                if(err) throw err;
+            ], function (err, results) {
+                if (err) throw err;
                 callback(results[1]);
             });
         }
