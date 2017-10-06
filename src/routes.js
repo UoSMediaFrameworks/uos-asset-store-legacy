9baf33576843fece1cc50ab899f165a65e89798e
Aaron Phillips <aaronp261@gmail.com>
2017-10-06 14:21:45 +0100

    Dynamically adding properties still failing.  Highly unusual now.

--- src/routes.js
+++ src/routes.js
@@ -255,7 +255,7 @@ module.exports = {
 
                             if(index !== -1) {
                                 console.log("forEach Result - assigning vmob");
-                                var mediaObject = _.cloneDeep(mediaScene.scene[index]);
+                                var mediaObject = _.clone(mediaScene.scene[index]);
                                 mediaObject["vmob"] = vmob;
                                 mediaScene.scene[index] = mediaObject;
                                 console.log("forEach Result - assigning mediaScene.scene[index]:", mediaScene.scene[index]);
