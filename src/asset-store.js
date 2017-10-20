f55e2819cce2dad1274a5557ddf182c70e50e10d
Aaron Phillips <aaronp261@gmail.com>
2017-10-20 14:58:04 +0100

    Small fix for interesting news CORS issue with uploading.

--- src/asset-store.js
+++ src/asset-store.js
@@ -41,6 +41,7 @@ var AssetStore = function (ops) {
     // APEP set cross origin headers to allow our different environment and DNS addresses access to the API
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
